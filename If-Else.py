23f0076ad6b5ed51921e955f074172eb9fe0e9c0
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-09-21 11:13:45 +0530

    Update If-Else.py

--- If-Else.py
+++ If-Else.py
@@ -1,38 +1,63 @@
 https://www.hackerrank.com/challenges/py-if-else/problem
-Question:
-Task
-Given an integer, , perform the following conditional actions:
+# Question:
+# Task
+# Given an integer, , perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Input Format
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
 
-A single line containing a positive integer, .
+# A single line containing a positive integer, .
 
-Constraints
+# Constraints
 
-Output Format
+# Output Format
 
-Print Weird if the number is weird. Otherwise, print Not Weird.
+# Print Weird if the number is weird. Otherwise, print Not Weird.
 
-Sample Input 0
+# Sample Input 0
 
-3
-Sample Output 0
+# 3
+# Sample Output 0
 
-Weird
-Explanation 0
+# Weird
+# Explanation 0
 
 
- is odd and odd numbers are weird, so print Weird.
+#  is odd and odd numbers are weird, so print Weird.
 
-Sample Input 1
+# Sample Input 1
 
-24
-Sample Output 1
+# 24
+# Sample Output 1
 
-Not Weird
+# Not Weird
+# Solution
+#!/bin/python
 
+import math
+import os
+import random
+import re
+import sys
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Main logic:
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n % 2 is 0:
+        if n in range(2 , 6):
+            print("Not Weird")
+        elif n in range(6  , 21):
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+        
+    else:
+        print("Weird")
 
